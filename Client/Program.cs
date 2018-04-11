aa02e294d27399b2a5dbd8acc3a8743edd5ee761
Sabina <sabina.orunbaeva@gmail.com>
2018-04-11 21:21:47 +0600

    Add files via upload

---
+++ Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public delegate void ClientCallable(Socket socket);
+    public class Client
+    {
+        private Socket socket;
+        public Client(byte[] ip, int port)
+        {
+            socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(new (new IPAddress(ip)),port);
+        }
+        public void Write(string Connection)
+        {
+
+        }
+        public void Call(ClientCallable callable)
+        {
+            callable(socket);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ NP_Lesson2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP_Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = 
+                new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            server.Bind(
+                new IPEndPoint(
+                    new IPAddress(
+                        new byte[] { 127, 0, 0, 1 }), 
+                    3080));
+
+            server.Listen(10);
+
+            var client = server.Accept();
+            byte[] buffer = new byte[10];
+            StringBuilder globalBuffer = new StringBuilder();
+
+            while (!client.Poll(1000, SelectMode.SelectRead)&&client.Connected)
+            {
+                var readed = client.Receive(buffer);
+                for(int i = 0; i < readed; i++)
+                {
+                    if (buffer[i]==13)
+                    {
+                        client.Send(
+                            Encoding.ASCII.GetBytes(
+                                $"Recieved text: {globalBuffer.ToString()}"));
+                        globalBuffer.Clear();
+                    }
+                    else
+                    {
+                        if (buffer[i] == 10) continue;
+                        globalBuffer.Append(Convert.ToChar(buffer[i]));
+                    }
+                }
+            }
+
+        }
+    }
+}
